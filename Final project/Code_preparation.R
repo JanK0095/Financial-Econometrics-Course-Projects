5647b2f35978a2ee77484d0399937c4d4cd8b12b
Jan Kubal <kubaljan@gmail.com>
2022-05-21 12:48:58 +0200

    Revert "Adjustments in Data Description"
    This reverts commit d321a10ae3a301b469d886ea276e7f6cc05c12d6.

--- Final project/Code_preparation.R
+++ Final project/Code_preparation.R
@@ -319,11 +319,8 @@ forecast_errors <- lapply(forecasts, calc_errors)
 ### Plotting forecast errors ###
 par(mfrow = c(3,2))
 for (i in 1:6) {
-  plot_holder <- plot(forecast_errors[[i]], main = model_names[i])
-  plot_holder <- addLegend("topright", on = 1, legend.names = c("Expanding window", "Rolling window"), col = c("black", "red"), lty = 1, bty = "n", lwd = c(2, 1))
-  print(plot_holder)
+  print(plot(forecast_errors[[i]], main = model_names[i]))
 }
-
 
 ### Calculating loss functions ###
 
